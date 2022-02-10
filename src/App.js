9928f227cc91760164be728f0c8bd99d7ad99892
Brian Chamberlain <chambbri@flip2.engr.oregonstate.edu>
2022-02-09 23:36:52 -0800

    converted html files to js and started setting up routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import animals from './pages/animals';
+import employees from './pages/employees';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hey Kyle!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the West Coast Best Coast Zoo</h1>
+        <Router>
+          <Route path="/animals" element={< animals/>}></Route>
+          <Route path="/animals" element={< employees/>}></Route>
+        </Router>
+        <Link to="/animals">Animals</Link>
+        <Link to="/employees">Employees</Link>
       </header>
     </div>
   );
