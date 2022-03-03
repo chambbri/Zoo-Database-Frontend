a4064fed19f249b098a968c58ed942ce02dd6fe3
Kyle Macabasco <kylemacabasco@gmail.com>
2022-03-03 12:15:59 -0800

    edited the Exhibit Page

--- src/pages/ExhibitPage.js
+++ src/pages/ExhibitPage.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import InsertExhibit from '../components/exhibits/InsertExhibit';
 import ExhibitList from '../components/exhibits/ExhibitList';
 
 
 function ExhibitPage() {
+
+    const [exhibits, setExhibits] = useState([]);
+    const [createExhibit, setCreateExhibit] = useState([])
+
+    const getExhibits = async() => {
+        const res = await fetch('/exhibits', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const exhibits = await res.json()
+        setExhibits(exhibits)
+    };
+
+    useEffect(() => {
+        getExhibits();
+    }, []);
+
 
 
     return (
