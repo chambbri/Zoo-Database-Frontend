5660b2f5b2b01df11a373a2a05acc9c721c6f155
Kyle Macabasco <kylemacabasco@gmail.com>
2022-03-03 04:12:27 -0800

    cleaned up the react ui code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
 function HomePage() {
+
+
+    const [exhibits, setExhibits] = useState([]);
+
+    const getExhibits = async () => {
+        const response = await fetch('localhost-serve:22130/exhibits')
+        const exhibits = await response.json();
+        setExhibits(exhibits)
+    }
+
+    useEffect(() => {
+        getExhibits();
+    }, [])
+
     return (
+
         <>
             <h1>West Coast Best Coast Zoo</h1>
             <Link to="/animals">List of Animals</Link>
