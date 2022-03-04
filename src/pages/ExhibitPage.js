4a79bb5f8bd160a13a35e813e43e6edb55a449ba
Brian Chamberlain <chambbri@flip1.engr.oregonstate.edu>
2022-03-03 23:56:40 -0800

    attempted to add create for exhibit, not working yet

--- src/pages/ExhibitPage.js
+++ src/pages/ExhibitPage.js
@@ -1,5 +1,4 @@
 import { React, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import InsertExhibit from '../components/exhibits/InsertExhibit';
 import ExhibitList from '../components/exhibits/ExhibitList';
 
@@ -7,7 +6,9 @@ import ExhibitList from '../components/exhibits/ExhibitList';
 function ExhibitPage() {
 
     const [exhibits, setExhibits] = useState([]);
-    const [createExhibit, setCreateExhibit] = useState([]);
+    const [type, setType] = useState('');
+    const [size, setSize] = useState('');
+    const [capacity, setCapcity] = useState('');
 
     const getExhibits = async() => {
         const res = await fetch('http://flip1.engr.oregonstate.edu:22131/exhibits');
@@ -19,13 +20,39 @@ function ExhibitPage() {
         getExhibits();
     }, []);
 
+    const addExhibit = async() => {
+        const newExhibit = {type, size, capacity};
+        const response = await fetch('http://flip1.engr.oregonstate.edu:22131/exhibits', {
+            method: 'POST',
+            body: JSON.stringify(newExhibit),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.status === 201){
+            alert("Successfully created the exhibit");
+        } else{
+            console.error(`Failed to create the exhibit. Status code ${response.status}`);
+        }
+    }
+
 
 
     return (
         <>
         <div className='app-container'>
             <h1>List of Exhibits</h1>
-            <InsertExhibit />
+            <form>
+            <fieldset>
+                <legend>Add an exhibit</legend>
+                <label>Type of Exhibit <input type="text" id="type" value={type} onChange = {e =>setType(e.target.value)}/></label>
+                <label>Size<input type="int" id="size" value={size} onChange = {e =>setSize(e.target.value)}/></label>
+                <br />
+                <label>Animal Capacity <input type="int" id="capacity" value={capacity} onChange = {e =>setCapcity(e.target.value)} /></label>
+                <br />
+                <button onClick={addExhibit}>Add</button>
+            </fieldset>
+            </form>
             <br />
             <ExhibitList exhibits={exhibits}/>
             <br />
