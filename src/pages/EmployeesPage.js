e6ade147b7fc9445a18550051a0ea4508356f02c
Kyle Macabasco <kylemacabasco@gmail.com>
2022-03-09 23:40:04 -0800

    edited EmployeesPage

--- src/pages/EmployeesPage.js
+++ src/pages/EmployeesPage.js
@@ -3,11 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import InsertEmployee from '../components/employees/InsertEmployee';
 import EmployeeList from '../components/employees/EmployeeList';
+import Axios from 'axios';
 
 function EmployeesPage() {
 
     const [employees, setEmployees] = useState([]);
-    const [createEmployees, setCreateExhibit] = useState([]);
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [job, setJob] = useState('');
 
     const getEmployees = async() => {
         const res = await fetch('http://flip1.engr.oregonstate.edu:22131/employees');
@@ -19,12 +24,32 @@ function EmployeesPage() {
         getEmployees();
     }, []);
 
+    const addEmployee = () => {
+        Axios.post('http://flip1.engr.oregonstate.edu:22131/employees', {
+            fname: fname,
+            lname: lname,
+            phone: phone,
+            email: email,
+            job_title: job,
+        }).then(() => {
+            alert("Successfully added Employees")
+        });
+    };
+
     return(
         <body>
             <h1>West Coast Best Coast Zoo Employees</h1>
-            <div>
-                <InsertEmployee />
-            </div>
+            <form>
+                <fieldset>
+                    <legend>Add an employee</legend>
+                    <label>First Name <input type="text" id="fname" value={fname} onChange={e =>setFname(e.target.value)} /></label>
+                    <label>Last Name <input type="text" id="lname" value={lname} onChange={e =>setLname(e.target.value)} /></label>
+                    <label>Phone Number <input type="tel" id="phone" value={phone} onChange={e =>setPhone(e.target.value)} /></label>
+                    <label>Email<input type="email" id="email" value={email} onChange={e =>setEmail(e.target.value)} /></label>
+                    <label>Job Title<input type="text" id="job" value={job} onChange={e =>setJob(e.target.value)} /></label>
+                    <button onClick={addEmployee}>Add</button>
+                </fieldset>
+            </form>
             <br />
             <div>
                 <EmployeeList employees={employees} />
